c68b284186c01223f06c16fed4ad135ad4ac7a17
kaBeech <contact@kaBeech.com>
2023-07-03 18:57:28 -0700

    Add Contact link

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,11 +5,19 @@ export default component$(() => {
   return (
     <div class="flex column alignCenter justifyCenter height100">
       <img
-        width={328}
-        height={328}
+        width={420}
+        height={420}
         src="QSL-symbol.webp"
         alt="The QSL symbol"
       />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        class="link button"
+        href="mailto:contact@qsl.art"
+      >
+        <h4 class="margin2">contact@qsl.art</h4>
+      </a>
     </div>
   );
 });
